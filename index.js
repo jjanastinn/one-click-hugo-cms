f3427e8f3d00d2253f32a848e000025506a3c1ab
Jana Stinn <jana-stinn@gmx.de>
2018-10-01 23:36:29 +0200

    added animation to about part

--- index.js
+++ index.js
@@ -1,31 +1,64 @@
 'use strict';
 
-var passingElement = document.querySelector("#contact");
-var anotherElement = document.querySelector("#contactimage");
+const steps = document.querySelectorAll(".experience");
+const brownline = document.querySelectorAll(".cv-line-brown");
+const greenline = document.querySelectorAll(".cv-line-green");
+const redline = document.querySelectorAll(".cv-line-red");
 
-function createObserver() {
-  var options = {
+
+const createObserver = () => {
+  const options = {
     root: null,
     rootMargin: "0px",
-    threshold: 1.0
+    threshold: .5
   };
 
-  var observer = new IntersectionObserver(handleIntersect, options);
-  observer.observe(passingElement);
+  const observer = new IntersectionObserver(handleIntersect, options);
+  steps.forEach(step => observer.observe(step));
 }
 
-function handleIntersect(entries, observer) {
-  entries.forEach(function(entry) {
-    if (entry.intersectionRatio < 1.0) {
-      entry.target.style.gridTemplateColumns = "1fr 1fr";
-      anotherElement.style.display = "none";
-    } else {
-      entry.target.style.gridTemplateColumns = "1fr 1fr 1fr";
-      anotherElement.style.display = "flex";
+const handleIntersect = (entries, observer) => {
+  entries.forEach(entry => {
+    if(entry.intersectionRatio > 0) {
+      entry.target.children[2].style.animation = "brownline 2s forwards";
+      entry.target.children[3].style.animation = "greenline 2s forwards";
+      entry.target.children[4].style.animation = "redline 2s forwards";
+      observer.unobserve(entry.target);
     }
   });
 }
 
-window.addEventListener("load", function(event) {
+window.addEventListener("load", (event) => {
   createObserver();
 }, false);
+
+
+// const passingElement = document.querySelector("#contact");
+// const anotherElement = document.querySelector("#contactimage");
+
+// const createObserver = () => {
+//   const options = {
+//     root: null,
+//     rootMargin: "0px",
+//     threshold: 1.0
+//   };
+
+//   const observer = new IntersectionObserver(handleIntersect, options);
+//   observer.observe(passingElement);
+// }
+
+// const handleIntersect = (entries, observer) => {
+//   entries.forEach(entry => {
+//     if (entry.intersectionRatio < 1.0) {
+//       entry.target.style.gridTemplateColumns = "1fr 1fr";
+//       anotherElement.style.display = "none";
+//     } else {
+//       entry.target.style.gridTemplateColumns = "1fr 1fr 1fr";
+//       anotherElement.style.display = "flex";
+//     }
+//   });
+// }
+
+// window.addEventListener("load", (event) => {
+//   createObserver();
+// }, false);
