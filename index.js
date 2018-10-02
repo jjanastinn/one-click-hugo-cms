35640eca0d1251ddb8c9bfa47b51e78be1524c3d
Jana Stinn <jana-stinn@gmx.de>
2018-10-02 14:53:31 +0200

    finished desktop version

--- index.js
+++ index.js
@@ -5,60 +5,90 @@ const brownline = document.querySelectorAll(".cv-line-brown");
 const greenline = document.querySelectorAll(".cv-line-green");
 const redline = document.querySelectorAll(".cv-line-red");
 
-
 const createObserver = () => {
   const options = {
     root: null,
     rootMargin: "0px",
     threshold: .5
   };
-
+  
   const observer = new IntersectionObserver(handleIntersect, options);
   steps.forEach(step => observer.observe(step));
 }
 
 const handleIntersect = (entries, observer) => {
   entries.forEach(entry => {
-    if(entry.intersectionRatio > 0) {
+    if (entry.intersectionRatio > 0) {
       entry.target.children[2].style.animation = "brownline 2s forwards";
       entry.target.children[3].style.animation = "greenline 2s forwards";
       entry.target.children[4].style.animation = "redline 2s forwards";
       observer.unobserve(entry.target);
-    }
+    } 
+  });
+}
+
+const contact = document.querySelector("#contact");
+
+const createAnotherObserver = () => {
+  const options = {
+    root: null,
+    rootMargin: "0px",
+    threshold: 1
+  };
+
+  const observer = new IntersectionObserver(handleAnotherIntersect, options);
+  observer.observe(contact);
+}
+
+const handleAnotherIntersect = (entries, observer) => {
+  entries.forEach(entry => {
+    if (entry.intersectionRatio > 0) {
+      entry.target.children[0].children[0].style.animation = "loadline 1s forwards .2s"
+      entry.target.children[0].children[1].style.animation = "loadline 1s forwards .3s"
+      entry.target.children[0].children[2].style.animation = "loadline 1s forwards .4s"
+      entry.target.children[0].children[3].style.animation = "loadline 1s forwards .5s"
+      entry.target.children[0].children[4].style.animation = "loadline 1s forwards .6s"
+      entry.target.children[0].children[5].style.animation = "loadline 1s forwards .7s"
+      entry.target.children[0].children[6].style.animation = "loadline 1s forwards .8s"
+      entry.target.children[0].children[7].style.animation = "loadline 1s forwards .9s"
+      entry.target.children[0].children[8].style.animation = "loadline 1s forwards 1.1s"
+      entry.target.children[0].children[9].style.animation = "loadline 1s forwards 1.1s"
+      entry.target.children[0].children[10].style.animation = "loadline 1s forwards 1.2s"
+      entry.target.children[0].children[11].style.animation = "loadline 1s forwards 1.3s"
+      entry.target.children[0].children[12].style.animation = "loadline 1s forwards 1.4s"
+      entry.target.children[0].children[13].style.animation = "loadline 1s forwards 1.5s"
+      entry.target.children[0].children[14].style.animation = "loadline 1s forwards 1.6s"
+      entry.target.children[0].children[15].style.animation = "loadline 1s forwards 1.7s"
+      entry.target.children[0].children[16].style.animation = "loadline 1s forwards 1.8s"
+    } 
+  });
+}
+
+const projectline = document.querySelector("#project-lines");
+
+const createYetAnotherObserver = () => {
+  const options = {
+    root: null,
+    rootMargin: "0px",
+    threshold: .5
+  };
+
+  const observer = new IntersectionObserver(handleYetAnotherIntersect, options);
+  observer.observe(projectline);
+}
+
+const handleYetAnotherIntersect = (entries, observer) => {
+  entries.forEach(entry => {
+    console.log(entry);
+    if (entry.intersectionRatio > 0) {
+      entry.target.children[2].style.animation = "projectline 1s forwards .2s"
+    } 
   });
 }
 
 window.addEventListener("load", (event) => {
   createObserver();
+  createAnotherObserver();
+  createYetAnotherObserver();
 }, false);
 
-
-// const passingElement = document.querySelector("#contact");
-// const anotherElement = document.querySelector("#contactimage");
-
-// const createObserver = () => {
-//   const options = {
-//     root: null,
-//     rootMargin: "0px",
-//     threshold: 1.0
-//   };
-
-//   const observer = new IntersectionObserver(handleIntersect, options);
-//   observer.observe(passingElement);
-// }
-
-// const handleIntersect = (entries, observer) => {
-//   entries.forEach(entry => {
-//     if (entry.intersectionRatio < 1.0) {
-//       entry.target.style.gridTemplateColumns = "1fr 1fr";
-//       anotherElement.style.display = "none";
-//     } else {
-//       entry.target.style.gridTemplateColumns = "1fr 1fr 1fr";
-//       anotherElement.style.display = "flex";
-//     }
-//   });
-// }
-
-// window.addEventListener("load", (event) => {
-//   createObserver();
-// }, false);
